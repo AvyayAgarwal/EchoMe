7c98007de9055bc0eeb61999fb11083f3c0735bf
Vinit Soni <vinitsoni9869@gmail.com>
2018-01-20 00:05:12 -0500

    add fake login to loginActivity

--- app/src/main/java/com/example/inos/echome/ui/LoginActivity.java
+++ app/src/main/java/com/example/inos/echome/ui/LoginActivity.java
@@ -1,15 +1,29 @@
 package com.example.inos.echome.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.inos.echome.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button mStubLoginBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mStubLoginBtn = (Button) findViewById(R.id.stub_login_btn);
+
+        mStubLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, QuestionFeedActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/inos/echome/ui/QuestionFeedActivity.java
@@ -0,0 +1,16 @@
+package com.example.inos.echome.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.inos.echome.R;
+
+public class QuestionFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_feed);
+        
+    }
+}
